adb5b0557454a934d10b01024d8f029e631d95a2
SaifulET <si912999@gmail.com>
2026-01-29 04:55:49 +0600

    2nd commit

--- src/router/Routes.jsx
+++ src/router/Routes.jsx
@@ -2,32 +2,18 @@ import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "../Layout/Main/Main";
 import SignIn from "../Pages/Auth/SignIn/SignIn";
 import ForgatePassword from "../Pages/Auth/ForgatePassword/ForgatePassword";
-import AboutUs from "../Pages/Settings/AboutUS/AboutUs";
-import PrivacyPolicy from "../Pages/Settings/PrivacyPolicy/PrivacyPolicy";
-import TermsCondition from "../Pages/Settings/TermsCondition/TermsCondition";
+
 import PrivateRoute from "./PrivateRoute";
 import Dashboard from "../Pages/Dashboard/Dashboard";
 import ProfilePage from "../Pages/AdminProfile/ProfilePage";
 import VerifyCode from "../Pages/Auth/VerifyCode/VerifyCode";
 import NewPass from "../Pages/Auth/NewPass/NewPass";
-import Notifications from "../Pages/Notifications/Notifications";
-import AnalysisPage from "../Pages/Analysis/AnalysisPage";
-import Subscriptions from "../Pages/Subscriptions/Subscriptions";
-import Analysis from "../Pages/Analysis/Analysis";
-import RestaurantRequest from "../Pages/RestaurantRequest/RestaurantRequest";
-import UserList from "../Pages/UserList/UserList";
-import Earnings from "../Pages/Earnings/Earnings";
-import Categories from "../Pages/Categories/Categories";
-import Reports from "../Pages/Reports/Reports";
+
+
 import Settings from "../Pages/Settings/Settings";
-import AllMessages from "../Pages/Messages/AllMessages";
 import BlockedList from "../Pages/BlockedList/BlockedList";
 import ChangePass from "../Pages/AdminProfile/ChangePass";
-import AdsSetup from "../Pages/AdsSetup/AdsSetup";
-import ActivityEvents from "../Pages/Activity & Events/ActivityEvents";
-import VenueListingApproval from "../Pages/Venue & Service/VenueListingApproval";
-import VenueApproval from "../Components/VenueandService/VenueDetails";
-import Veneudetails from "../Pages/Venue & Service/veneudetails";
+
 import AddEmployee from "../Pages/AddEmployee/AddEmployee";
 import ProjectTable from "../Pages/Project/Project";
 import BuildingFloorLayout from "../Pages/ProjectDetails/ProjectDetails";
@@ -65,27 +51,14 @@ export const router = createBrowserRouter([
           { path: "/block-list", element: <BlockedList /> },
           { path: "/project/:id", element: <BuildingFloorLayout /> },
 
-          { path: "/earnings", element: <Earnings /> },
-          { path: "/restaurant-request", element: <RestaurantRequest /> },
-          { path: "/analysis-page", element: <AnalysisPage /> },
-          { path: "/analysis/:id", element: <Analysis /> },
-          { path: "/subscriptions", element: <Subscriptions /> },
-          { path: "/categories", element: <Categories /> },
-          { path: "/reports", element: <Reports /> },
-          { path: "/ads-setup", element: <AdsSetup /> },
-            { path: "/activity&events", element: <ActivityEvents /> },
-          { path: "/notifications", element: <Notifications /> },
+         
           { path: "/settings", element: <Settings /> },
-          { path: "/settings/about-us", element: <AboutUs /> },
-          { path: "/settings/privacy-policy", element: <PrivacyPolicy /> },
-          { path: "/settings/terms-condition", element: <TermsCondition /> },
+       
           { path: "/settings/profile", element: <ProfilePage /> },
           { path: "/settings/change-password", element: <ChangePass /> },
           { path: "/settings/forget-password", element: <ForgotPassword /> },
           { path: "/settings/email-verification", element: <EmailVerification /> },
-          { path: "/messages", element: <AllMessages /> },
-          { path: "/venueandservice", element: <VenueListingApproval /> },
-          { path: "/venueandservice/venuedetails/:id", element: <Veneudetails/> },
+         
         ],
       },
     ],
